43d9ccde3341ce65e1023ce737c1a5efe4e44ce0
Muhammad Zeeshan Khalid <52278972+mzkhalid039@users.noreply.github.com>
2023-04-18 16:04:22 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from os.path import abspath, dirname
+
+setup(
+    name='DWBuilder',
+    version='1.0.0',
+    author='M.Z.Khalid',
+    author_email='muhammad.z.khalid@ntnu.no',
+    description='A package for developing domain wall structures for atomistic calculations',
+    url='https://github.com/mzkhalid039/DWBuilder',
+    license='MIT',
+    packages=find_packages(),
+    install_requires=['ase','numpy','matplotlib'],
+    entry_points={
+        'console_scripts': [
+            'dwbuilder = scripts.dwbuilder:main',
+            'dbuilder = scripts.dbuilder:main',
+        ]
+    }
+)
+
