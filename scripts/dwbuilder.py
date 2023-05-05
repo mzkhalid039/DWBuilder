f046820c281df7ab18f67e6dd19c3b0a075dc3b2
Muhammad Zeeshan Khalid <52278972+mzkhalid039@users.noreply.github.com>
2023-05-05 15:15:15 +0200

    Update dwbuilder.py

--- scripts/dwbuilder.py
+++ scripts/dwbuilder.py
@@ -35,26 +35,32 @@ elif data["international"] == "P6_3cm":
     system_selection = 4
 elif data["international"] == "Pnma":
     system_selection = 5
+elif data["international"] == "Pmc2_1":
+    system_selection = 6
 
 else:
     print("Warning: The international symbol does not match any pre-defined systems.")
     print("Please manually select the system.")
-    system_selection = 6
+    system_selection = 7
 
 # Prompt the user to select the system if not automatically selected
-if system_selection == 6:
+if system_selection == 7:
     print("Select the system:")
     print("1 - R3c")
     print("2 - R3m")
     print("3 - P4mm")
     print("4 - P6_3cm")
     print("5 - Pnma")
-    print("6 - Interface/Dws with known ORs")
+    print("6 - Pmc2_1")
+
+    print("7 - Manual Orientation relationships")
 
 
     system_selection = int(input())
 
-# BFO system
+# BFO system 
+# Reference: Wang Y, Nelson C, Melville A, Winchester B, Shang S, Liu ZK, Schlom DG, Pan X, Chen LQ. BiFeO 3 domain wall energies and structures: a combined experimental and density functional theory+ U study. Physical review letters. 2013 Jun 24;110(26):267601.
+
 if system_selection == 1:
 
     # Cut and rotate the structure to obtain a pseudo-cubic structure
@@ -144,6 +150,8 @@ if system_selection == 1:
 print("Domain wall structures and supercells created successfully!")
 
  # BaTiO3 system
+# Ref: Taherinejad M, Vanderbilt D, Marton P, Stepkova V, Hlinka J. Bloch-type domain walls in rhombohedral BaTiO 3. Physical Review B. 2012 Oct 22;86(15):155138.
+
 if system_selection == 2:
     # Prompt the user to enter the input file name
 
@@ -230,7 +238,8 @@ if system_selection == 2:
 
 
 
- # PTO system
+ # PTO system 
+# Ref: 
 if system_selection == 3:
     # Prompt the user to enter the input file name
     
@@ -361,6 +370,7 @@ if system_selection == 4:
 
 
  # CTO system
+# Ref: Barone P, Di Sante D, Picozzi S. Improper origin of polar displacements at CaTiO 3 and CaMnO 3 twin walls. Physical Review B. 2014 Apr 8;89(14):144104.
 if system_selection == 5:
     # Prompt the user to enter the input file name
 
@@ -385,9 +395,235 @@ if system_selection == 5:
     print("Domain 2 Orientation (R180 DW): a = [-1, 1, 0), b = [0, 0, 1], c = (1, 1, 0)")
     print("\033[1;31;40mWarning: This code might create domain wall artifacts, like missing or duplicate atoms, at the domain wall. Therefore, manual adjustment may be needed.\033[0m")
 
-    
-# Manually develop interfaces/DW if you have ORs
+    # Create supercells of the domain walls
+
+    supercell_size = int(input("Enter size of supercell (in number of unit cells): "))
+    if angle_selection == 5:
+        slab180_supercell = slab180.repeat([supercell_size, supercell_size, 1])
+        slab180_supercell.write(os.path.join(current_path, 'Pnma', 'T180_supercell.vasp'), sort=True, vasp5=True)
+
+    #Print confirmation message to user
+
+    print("Domain wall structures and supercells created successfully!")
+
+     # Orthorhombic system
+
 if system_selection == 6:
+    # Prompt the user to select the domain wall angle
+    print("Select the domain wall angle:")
+    print("1 - O120 degrees")
+    print("2 - O180 degrees")
+    print("3 - O90 degrees")
+    print("4 - All angles")
+    angle_selection = int(input())
+
+    # Find the path of the current directory
+    current_path = os.getcwd()
+    # Read the input file
+
+    axis = input("Please select a polar axis (a, b, or c): ")
+
+    if axis == 'a':
+        slab1 = cut(D1, a=[1, 0, 0], b=[0.0, 1.0, 0], c=[0, 0, 1])
+        rotate(slab1, slab1.cell[0], (0,1,0), slab1.cell[1], (1,0,0))
+
+    elif axis == 'b':
+        slab1 = cut(D1, a=[0, 1, 0], b=[0.0, 0.0, 1], c=[1, 0, 0])
+        rotate(slab1, slab1.cell[0], (0,1,0), slab1.cell[1], (1,0,0))
+
+    elif axis == 'c':
+        slab1 = cut(D1, a=[0, 0, 1], b=[1.0, 0.0, 0], c=[0, 1, 0])
+        rotate(slab1, slab1.cell[0], (0,1,0), slab1.cell[1], (1,0,0))
+
+    else:
+        print("Default c axis was selected")
+        slab1 = cut(D1, a=[0, 0, 1], b=[1.0, 0.0, 0], c=[0, 1, 0])
+        rotate(slab1, slab1.cell[0], (0,1,0), slab1.cell[1], (1,0,0))
+    
+    slab1.write('rot.vasp', sort=True, vasp5=True)
+
+    Rotation = ase.io.read("rot.vasp")
+    domain_size = int(input("Enter the domain wall size (in number of unit cells): "))
+
+
+
+    if angle_selection == 1 or angle_selection == 4:
+    #120 DW H-H and T-T domain walls
+        slab2 = cut(Rotation, a=[-1, 2, 0], b=[-2.0*domain_size, -1.0*domain_size, 0], c=[0, 0, 1])
+        rotate(slab2, slab2.cell[0], (0,1,0), slab2.cell[1], (1,0,0))
+
+
+        slab3 = cut(Rotation, a=[-1, -2, 0], b=[2.0*domain_size, -1.0*domain_size, 0], c=[0, 0, 1])
+        rotate(slab3, slab3.cell[0], (0,1,0), slab3.cell[1], (1,0,0))
+
+        #OR for domain 1
+        a1 = [-1, 2, 0]
+        b1 = (-1, 2, 0)
+        c1 = [0, 0, 1]
+        print ("a b c")
+        print("{} {} {}".format (a1, b1, c1))
+
+        #OR for domain 2
+        a2 = [-1, -2, 0]
+        b2 = (2, -1, 0)
+        c2 = [0, 0, 1]
+        print ("a b c")
+        print("{} {} {}".format (a2, b2, c2))
+        print ("This OR create H-H and T-T domain walls")
+
+
+        slab2.write('120_D1.vasp', sort=True, vasp5=True)
+        slab3.write('120_D2.vasp', sort=True, vasp5=True)
+
+        slab120hh = stack(slab3, slab2, axis=1, maxstrain=None)
+        os.makedirs(os.path.join(current_path, 'O120'))
+        slab120hh.write(os.path.join(current_path, 'O120', '120_HH_TT_DW.vasp'), sort=True, vasp5=True)
+        slab2.write(os.path.join(current_path, 'O120', 'D1.vasp'), sort=True, vasp5=True)
+        slab3.write(os.path.join(current_path, 'O120', 'D2.vasp'), sort=True, vasp5=True)
+
+        print("\033[1;31;40mWarning: This code generates duplicate atoms at the domain wall, use VESTA to remove duplicated or closer atoms (with 0.5Å cuttoff).\033[0m")
+
+
+
+    if angle_selection == 1 or angle_selection == 4:
+
+        #120 DW H-T domain walls
+
+        slab2 = cut(Rotation, a=[-1*domain_size, 2*domain_size, 0], b=[-2.0, -1.0, 0], c=[0, 0, 1])
+        rotate(slab2, slab2.cell[0], (0,1,0), slab2.cell[1], (1,0,0))
+
+        #slab2 generates (110) plane
+
+        slab3 = cut(Rotation, a=[-1*domain_size, -2*domain_size, 0], b=[2.0, -1.0, 0], c=[0, 0, 1])
+        rotate(slab3, slab3.cell[0], (0,1,0), slab3.cell[1], (1,0,0))
+
+        #OR for domain 1
+        a1 = [-1, 2, 0]
+        b1 = (-2, -1, 0)
+        c1 = [0, 0, 1]
+        print ("a b c")
+        print("{} {} {}".format (a1, b1, c1))
+
+        #OR for domain 2
+        a2 = [-1, -2, 0]
+        b2 = (2, -1, 0)
+        c2 = [0, 0, 1]
+        print ("a b c")
+        print("{} {} {}".format (a2, b2, c2))
+        print ("This OR create H-T domain walls")
+
+
+        slab2.write('120_D1.vasp', sort=True, vasp5=True)
+        slab3.write('120_D2.vasp', sort=True, vasp5=True)
+
+        slab120HT = stack(slab3, slab2, axis=0, maxstrain=None)
+        os.makedirs(os.path.join(current_path, 'O120_HT'))
+        slab120HT.write(os.path.join(current_path, 'O120_HT', '120_HT_DW.vasp'), sort=True, vasp5=True)
+        slab2.write(os.path.join(current_path, 'O120_HT', 'D1_120.vasp'), sort=True, vasp5=True)
+        slab3.write(os.path.join(current_path, 'O120_HT', 'D2_120.vasp'), sort=True, vasp5=True)
+
+
+        print("\033[1;31;40mWarning: This code generates duplicate atoms at the domain wall, use VESTA to remove duplicated or closer atoms (with 0.5Å cuttoff).\033[0m")
+
+
+
+    if angle_selection == 2 or angle_selection == 4:
+
+        #180 DW 
+
+        slab2 = cut(Rotation, a=[1, 0, 0], b=[0, domain_size, 0], c=[0, 0, 1])
+        rotate(slab2, slab2.cell[0], (0,1,0), slab2.cell[1], (1,0,0))
+
+        #slab2 generates (110) plane
+
+        slab3 = cut(Rotation, a=[-1, 0, 0], b=[0, -domain_size, 0], c=[0, 0, 1])
+        rotate(slab3, slab3.cell[0], (0,1,0), slab3.cell[1], (1,0,0))
+
+        #OR for 180 domain 1
+        a1 = [1, 0, 0]
+        b1 = (0, 1, 0)
+        c1 = [0, 0, 1]
+        print ("a b c")
+        print("{} {} {}".format (a1, b1, c1))
+    
+        #OR for 180 domain 2
+        a2 = [-1, 0, 0]
+        b2 = (0, -1, 0)
+        c2 = [0, 0, 1]
+        print ("a b c")
+        print("{} {} {}".format (a2, b2, c2))
+        print ("This OR create 180 domain walls")
+
+
+        slab2.write('180_D1.vasp', sort=True, vasp5=True)
+        slab3.write('180_D2.vasp', sort=True, vasp5=True)
+
+        slab180 = stack(slab3, slab2, axis=1, maxstrain=None)
+        os.makedirs(os.path.join(current_path, 'O180'))
+        slab180.write(os.path.join(current_path, 'O180', '180_DW.vasp'), sort=True, vasp5=True)
+        slab2.write(os.path.join(current_path, 'O180', 'D1_180.vasp'), sort=True, vasp5=True)
+        slab3.write(os.path.join(current_path, 'O180', 'D2_180.vasp'), sort=True, vasp5=True)
+
+
+        print("\033[1;31;40mCaution: It is always best practice to visualize structure.\033[0m")
+   
+    if angle_selection == 3 or angle_selection == 4:
+    #90 DW 
+
+        slab2 = cut(Rotation, a=[0, 1*domain_size, 0], b=[-1, 0, 0], c=[0, 0, 1])
+        rotate(slab2, slab2.cell[0], (0,1,0), slab2.cell[1], (1,0,0))
+
+        #slab2 generates (110) plane
+
+        slab3 = cut(Rotation, a=[-1*domain_size, 0, 0], b=[0, -1, 0], c=[0, 0, 1])
+        rotate(slab3, slab3.cell[0], (0,1,0), slab3.cell[1], (1,0,0))
+
+        #OR for 90 domain 1
+        a1 = [0, 1, 0]
+        b1 = (-1, 0, 0)
+        c1 = [0, 0, 1]
+        print ("a b c")
+        print("{} {} {}".format (a1, b1, c1))
+
+        #OR for 90 domain 2
+        a2 = [-1, 0, 0]
+        b2 = (0, -1, 0)
+        c2 = [0, 0, 1]
+        print ("a b c")
+        print("{} {} {}".format (a2, b2, c2))
+        print ("This OR create 90 domain walls")
+
+
+        slab2.write('90_D1.vasp', sort=True, vasp5=True)
+        slab3.write('90_D2.vasp', sort=True, vasp5=True)
+
+        slab90 = stack(slab3, slab2, axis=0, maxstrain=None)
+
+        os.makedirs(os.path.join(current_path, 'O90'))
+        slab90.write(os.path.join(current_path, 'O90', '90_DW.vasp'), sort=True, vasp5=True)
+        slab2.write(os.path.join(current_path, 'O90', 'D1_90.vasp'), sort=True, vasp5=True)
+        slab3.write(os.path.join(current_path, 'O90', 'D2_90.vasp'), sort=True, vasp5=True)
+
+
+
+        print("\033[1;31;40mCaution: It is always best practice to visualize structure.\033[0m")
+
+        supercell_size = int(input("Enter size of supercell (in number of unit cells): "))
+        if angle_selection == 1 or angle_selection == 4:
+            slab120hh_supercell = slab120hh.repeat([supercell_size, 1, supercell_size])
+            slab120hh_supercell.write(os.path.join(current_path, 'O120', 'O120HH_supercell.vasp'), sort=True, vasp5=True)
+        if angle_selection == 1 or angle_selection == 4:
+            slab120HT_supercell = slab120HT.repeat([1, supercell_size, supercell_size])
+            slab120HT_supercell.write(os.path.join(current_path, 'O120_HT', 'O120HT_supercell.vasp'), sort=True, vasp5=True)
+        if angle_selection == 2 or angle_selection == 4:
+            slab180_supercell = slab180.repeat([supercell_size, 1, supercell_size])
+            slab180_supercell.write(os.path.join(current_path, 'O180', 'O180_supercell.vasp'), sort=True, vasp5=True)
+        if angle_selection == 3 or angle_selection == 4:
+            slab90_supercell = slab90.repeat([1, supercell_size, supercell_size])
+            slab90_supercell.write(os.path.join(current_path, 'O90', 'O180_supercell.vasp'), sort=True, vasp5=True)
+
+# Manually develop interfaces/DW if you have ORs
+if system_selection == 7:
     def create_domain_wall(filename1, filename2, domain_size):
 
     # Find the path of the current directory
