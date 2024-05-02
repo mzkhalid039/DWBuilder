578897edf92f1c22b26fb79a655f992f6432f380
Muhammad Zeeshan Khalid <52278972+mzkhalid039@users.noreply.github.com>
2024-05-02 11:11:22 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from os.path import abspath, dirname
 setup(
     name='dwbuilder',
     version='1.1.0',
-    authors='M.Z.Khalid', 'S.M.Selbach'
+    authors = 'M.Z.Khalid, S.M.Selbach'
     author_email='zeeshan.khalid039@gmail.com',
     description='A package for developing domain wall structures for atomistic calculations',
     license='MIT',
