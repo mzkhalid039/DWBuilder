1578528c13d87e60d9b7b88e1f9311b2701eea29
Muhammad Zeeshan Khalid <52278972+mzkhalid039@users.noreply.github.com>
2024-05-30 10:30:54 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,14 +8,21 @@ setup(
     author_email='zeeshan.khalid039@gmail.com',
     description='A package for developing domain wall structures for atomistic calculations',
     license='MIT',
+    url='https://github.com/mzkhalid039/DWBuilder',
     packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
     install_requires=['ase','numpy','matplotlib', 'pymatgen', 'colorama'],
     entry_points={
         'console_scripts': [
             'dwbuilder = scripts.dwbuilder:main',
             'dbuilder = scripts.dbuilder:main',
             'hibuilder = scripts.hibuilder:main',
-
+            'polarization = scripts.polarization:main',
         ]
     }
 )
